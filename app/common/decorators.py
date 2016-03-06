d39535376fa9eaea10dd1c5de400b514979f5a66
Oleg Musaev <olegmusaev@gmail.com>
2016-03-06 19:47:47 +0300

    Removed not needed decorator

--- app/common/decorators.py
+++ app/common/decorators.py
@@ -13,7 +13,6 @@ from settings import DBSession
 
 __all__ = (
     'db_transaction',
-    'data_required',
 )
 
 
@@ -32,34 +31,5 @@ def db_transaction(func):
             DBSession.remove()
 
         return result
-
-    return wrapper
-
-
-def data_required(func):
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        req = args[1]
-        resp = args[2]
-        request_data = copy.deepcopy(json.load(req.stream))
-
-        validator = Schema(args[0].data_schema)
-
-        try:
-            validator(request_data)
-        except Invalid as e:
-            resp.status = falcon.HTTP_409
-            resp.body = json.dumps(
-                {
-                    'error': str(e),
-                    'data': request_data
-                }
-            )
-            return
-
-        kwargs.update({'data': request_data})
-
-        return func(*args, **kwargs)
 
     return wrapper
