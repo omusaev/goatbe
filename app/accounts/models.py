3fbf66aebb0abd97eb39f3a9bb3697b730f4d1e1
denis shushkevich <denis.shushkevich@gmail.com>
2016-03-10 11:51:20 +0300

    update account model

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -5,6 +5,7 @@ import logging
 
 from sqlalchemy import Column, BigInteger, String, DateTime, Enum
 from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.sql.schema import UniqueConstraint
 
 from db.base import Base
 import settings as app_settings
@@ -21,8 +22,18 @@ class Account(Base):
 
     __tablename__ = 'account'
 
+    class STATUS:
+        ACTIVE = 'active'
+        INACTIVE = 'inactive'
+
+        FIELDS = (
+            ACTIVE,
+            INACTIVE,
+        )
+
     id = Column(BigInteger, primary_key=True, nullable=False)
     name = Column(String(255), nullable=False, default='')
+    status = Column(Enum(*STATUS.FIELDS, name='account_status'), nullable=False, default=STATUS.ACTIVE)
     created_at = Column(DateTime, default=datetime.datetime.now)
     updated_at = Column(DateTime, onupdate=datetime.datetime.now)
     auth_method = Column(
@@ -31,3 +42,5 @@ class Account(Base):
     )
     identifier = Column(String(255), nullable=False)
     attributes = Column(JSON)
+
+    UniqueConstraint('auth_method', 'identifier', name='auth_provider_user')
