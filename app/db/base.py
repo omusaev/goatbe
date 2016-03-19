38a50c26208d939e2faf66310b42e8cca6df5a9d
denis shushkevich <denis.shushkevich@gmail.com>
2016-03-20 01:06:09 +0300

    rewrite all migrations. flush DB required!

--- app/db/base.py
+++ app/db/base.py
@@ -25,5 +25,6 @@ def get_base_for_migrations():
     # we have to import all models here to use auto generation of migrations feature
     from common.sessions.models import Session
     from accounts.models import Account
+    from events.models import Event, Step, Participant, Assignee
 
     return Base
