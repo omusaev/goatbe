375b56659fd745ccea5347e098f21ca658f35415
denis shushkevich <denis.shushkevich@gmail.com>
2016-03-26 23:15:19 +0300

    up docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ author = u'Hikapro Team'
 # built documents.
 #
 # The short X.Y version.
-version = u'1.0'
+version = u'1.0.1'
 # The full version, including alpha/beta/rc tags.
 release = u'1.0.1'
 
