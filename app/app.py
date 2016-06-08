971ab428827cb6159c8fa15a52996dde763aaa85
denis shushkevich <denis.shushkevich@gmail.com>
2016-06-08 20:49:09 +0300

    optimize running commands

--- app/app.py
+++ app/app.py
@@ -1,21 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import falcon
-
-from core.helpers import collect_installed_resources, collect_middlewares
-
-__all__ = (
-    'application',
-)
-
-
-application = falcon.API(middleware=list(collect_middlewares()))
-
-for resource in collect_installed_resources():
-    application.add_route(resource.url, resource)
-
 if __name__ in ('__main__',):
     from management import handle_cli
 
     handle_cli()
     exit()
+else:
+    import falcon
+
+    from core.helpers import collect_installed_resources, collect_middlewares
+
+    application = falcon.API(middleware=list(collect_middlewares()))
+
+    for resource in collect_installed_resources():
+        application.add_route(resource.url, resource)
