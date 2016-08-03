f76a7a14eeb366cd4e3939d91903fdcf6cb008b8
Oleg Musaev <olegmusaev@gmail.com>
2016-08-03 20:40:43 +0300

    Added some cli methods

--- cli/cli.py
+++ cli/cli.py
@@ -85,22 +85,10 @@ class GoatClient(object):
 
         self.out(data.get('data'))
 
-    def create_anonym(self, args):
-        url = self.url('/accounts/auth/anonym/')
-
-        response = self.make_request(url, {})
-
-        self.out('Session id: %s' % response.cookies.get('sessionid'))
-
-        if args.save:
-            self.save_session_id(response.cookies.get(self.SESSION_COOKIE_NAME))
-
-        return response
-
     def auth_anonym(self, args):
         url = self.url('/accounts/auth/anonym/')
 
-        data = {'user_access_token': args.token}
+        data = {'user_access_token': args.user_access_token}
 
         response = self.make_request(url, data)
 
@@ -110,6 +98,27 @@ class GoatClient(object):
             self.save_session_id(response.cookies.get(self.SESSION_COOKIE_NAME))
 
         return response
+
+    def auth_facebook(self, args):
+        url = self.url('/accounts/auth/facebook/')
+
+        data = {'user_access_token': args.user_access_token}
+
+        response = self.make_request(url, data)
+
+        self.out('Session id: %s' % response.cookies.get('sessionid'))
+
+        if args.save:
+            self.save_session_id(response.cookies.get(self.SESSION_COOKIE_NAME))
+
+        return response
+
+    def logout(self, args):
+        url = self.url('/accounts/logout/')
+
+        data = {}
+
+        return self.make_request(url, data)
 
     def events_types(self, args):
         url = self.url('/events/types/')
@@ -265,12 +274,182 @@ class GoatClient(object):
 
         return self.make_request(url, data)
 
+    def update_feedback(self, args):
+        url = self.url('/feedbacks/update/')
+
+        data = {
+            'event_id': args.event_id,
+            'feedback_id': args.feedback_id,
+            'comment': args.comment,
+            'rating': args.rating,
+        }
+
+        return self.make_request(url, data)
+
+    def feedback_details(self, args):
+        url = self.url('/feedbacks/details/')
+
+        data = {
+            'event_id': args.event_id,
+            'feedback_id': args.feedback_id,
+        }
+
+        return self.make_request(url, data)
+
+    def delete_feedback(self, args):
+        url = self.url('/feedbacks/delete/')
+
+        data = {
+            'event_id': args.event_id,
+            'feedback_id': args.feedback_id,
+        }
+
+        return self.make_request(url, data)
+
     def update_account(self, args):
         url = self.url('/accounts/update/')
 
         data = {
             'name': args.name,
         }
+
+        return self.make_request(url, data)
+
+    def create_step(self, args):
+        url = self.url('/steps/create/')
+
+        data = {
+            'event_id': args.event_id,
+            'title': args.title,
+            'description': args.description,
+            'type': args.type,
+            'order': args.order,
+        }
+
+        return self.make_request(url, data)
+
+    def update_step(self, args):
+        url = self.url('/steps/update/')
+
+        data = {
+            'event_id': args.event_id,
+            'step_id': args.step_id,
+            'title': args.title,
+            'description': args.description,
+            'order': args.order,
+        }
+
+        return self.make_request(url, data)
+
+    def step_details(self, args):
+        url = self.url('/steps/details/')
+
+        data = {
+            'event_id': args.event_id,
+            'step_id': args.step_id,
+        }
+
+        return self.make_request(url, data)
+
+    def delete_step(self, args):
+        url = self.url('/steps/delete/')
+
+        data = {
+            'event_id': args.event_id,
+            'step_id': args.step_id,
+        }
+
+        return self.make_request(url, data)
+
+    def create_participant(self, args):
+        url = self.url('/participants/create/')
+
+        data = {
+            'event_id': args.event_id,
+            'event_secret': args.event_secret,
+        }
+
+        return self.make_request(url, data)
+
+    def activate_participant(self, args):
+        url = self.url('/participants/activate/')
+
+        data = {
+            'event_id': args.event_id,
+        }
+
+        return self.make_request(url, data)
+
+    def delete_participant(self, args):
+        url = self.url('/participants/delete/')
+
+        data = {
+            'event_id': args.event_id,
+            'account_id': args.account_id,
+        }
+
+        return self.make_request(url, data)
+
+    def create_place(self, args):
+        url = self.url('/places/create/')
+
+        data = {
+            'event_id': args.event_id,
+            'title': args.title,
+            'description': args.description,
+            'start_at': args.start_at,
+            'finish_at': args.finish_at,
+            'order': args.order,
+            'point': {
+                'lng': args.lng,
+                'lat': args.lat,
+            }
+        }
+
+        return self.make_request(url, data)
+
+    def update_place(self, args):
+        url = self.url('/places/update/')
+
+        data = {
+            'event_id': args.event_id,
+            'title': args.title,
+            'description': args.description,
+            'start_at': args.start_at,
+            'finish_at': args.finish_at,
+            'order': args.order,
+            'point': {
+                'lng': args.lng,
+                'lat': args.lat,
+            }
+        }
+
+        return self.make_request(url, data)
+
+    def place_details(self, args):
+        url = self.url('/places/details/')
+
+        data = {
+            'event_id': args.event_id,
+            'place_id': args.place_id,
+        }
+
+        return self.make_request(url, data)
+
+    def delete_place(self, args):
+        url = self.url('/places/delete/')
+
+        data = {
+            'event_id': args.event_id,
+            'place_id': args.place_id,
+        }
+
+        return self.make_request(url, data)
+
+    def places_map(self, args):
+        url = self.url('/places/map/')
+
+        data = {}
 
         return self.make_request(url, data)
 
@@ -356,22 +535,43 @@ def add_feedback_parsers(sub_parsers):
     create_feedback_parser.add_argument('--rating', type=int)
     create_feedback_parser.set_defaults(handler='create_feedback')
 
+    update_feedback_parser = sub_parsers.add_parser('update_feedback')
+    update_feedback_parser.add_argument('--event_id', type=int)
+    update_feedback_parser.add_argument('--feedback_id', type=int)
+    update_feedback_parser.add_argument('--comment')
+    update_feedback_parser.add_argument('--rating', type=int)
+    update_feedback_parser.set_defaults(handler='update_feedback')
+
+    feedback_details_parser = sub_parsers.add_parser('feedback_details')
+    feedback_details_parser.add_argument('--event_id', type=int)
+    feedback_details_parser.add_argument('--feedback_id', type=int)
+    feedback_details_parser.set_defaults(handler='feedback_details')
+
+    delete_feedback_parser = sub_parsers.add_parser('delete_feedback')
+    delete_feedback_parser.add_argument('--event_id', type=int)
+    delete_feedback_parser.add_argument('--feedback_id', type=int)
+    delete_feedback_parser.set_defaults(handler='delete_feedback')
+
 
 def add_auth_parsers(sub_parsers):
 
     flush_session_parser = sub_parsers.add_parser('flush_session')
     flush_session_parser.set_defaults(handler='flush_session')
 
-    create_anonym_parser = sub_parsers.add_parser('create_anonym')
-    create_anonym_parser.add_argument('-s', '--save', help='Save session id cookie to use in future requests',
-                                      default=True)
-    create_anonym_parser.set_defaults(handler='create_anonym')
-
     auth_anonym_parser = sub_parsers.add_parser('auth_anonym')
-    auth_anonym_parser.add_argument('token')
+    auth_anonym_parser.add_argument('user_access_token')
     auth_anonym_parser.add_argument('-s', '--save', help='Save session id cookie to use in future requests',
                                     default=True)
     auth_anonym_parser.set_defaults(handler='auth_anonym')
+
+    auth_facebook_parser = sub_parsers.add_parser('auth_facebook')
+    auth_facebook_parser.add_argument('user_access_token')
+    auth_facebook_parser.add_argument('-s', '--save', help='Save session id cookie to use in future requests',
+                                    default=True)
+    auth_facebook_parser.set_defaults(handler='auth_facebook')
+
+    logout_parser = sub_parsers.add_parser('logout_parser')
+    logout_parser.set_defaults(handler='logout_parser')
 
 
 def add_accounts_parsers(sub_parsers):
@@ -379,6 +579,92 @@ def add_accounts_parsers(sub_parsers):
     update_account_parser = sub_parsers.add_parser('update_account')
     update_account_parser.add_argument('--name')
     update_account_parser.set_defaults(handler='update_account')
+
+
+def add_step_parsers(sub_parsers):
+
+    create_step_parser = sub_parsers.add_parser('create_step')
+    create_step_parser.add_argument('--event_id', type=int)
+    create_step_parser.add_argument('--type', default='CUSTOM')
+    create_step_parser.add_argument('--title')
+    create_step_parser.add_argument('--description')
+    create_step_parser.add_argument('--order', type=int)
+    create_step_parser.set_defaults(handler='create_step')
+
+    update_step_parser = sub_parsers.add_parser('update_step')
+    update_step_parser.add_argument('--event_id', type=int)
+    update_step_parser.add_argument('--step_id', type=int)
+    update_step_parser.add_argument('--title')
+    update_step_parser.add_argument('--description')
+    update_step_parser.add_argument('--order', type=int)
+    update_step_parser.set_defaults(handler='update_step')
+
+    step_details_parser = sub_parsers.add_parser('step_details')
+    step_details_parser.add_argument('--event_id', type=int)
+    step_details_parser.add_argument('--step_id', type=int)
+    step_details_parser.set_defaults(handler='step_details')
+
+    delete_step_parser = sub_parsers.add_parser('delete_step')
+    delete_step_parser.add_argument('--event_id', type=int)
+    delete_step_parser.add_argument('--step_id', type=int)
+    delete_step_parser.set_defaults(handler='delete_step')
+
+
+def add_participant_parsers(sub_parsers):
+
+    create_participant_parser = sub_parsers.add_parser('create_participant')
+    create_participant_parser.add_argument('--event_id', type=int)
+    create_participant_parser.add_argument('--event_secret')
+    create_participant_parser.set_defaults(handler='create_participant')
+
+    activate_participant_parser = sub_parsers.add_parser('activate_participant')
+    activate_participant_parser.add_argument('--event_id', type=int)
+    activate_participant_parser.set_defaults(handler='activate_participant')
+
+    delete_participant_parser = sub_parsers.add_parser('delete_participant')
+    delete_participant_parser.add_argument('--event_id', type=int)
+    delete_participant_parser.add_argument('--account_id', type=int)
+    delete_participant_parser.set_defaults(handler='delete_participant')
+
+
+def add_place_parsers(sub_parsers):
+
+    create_place_parser = sub_parsers.add_parser('create_place')
+    create_place_parser.add_argument('--event_id', type=int)
+    create_place_parser.add_argument('--title')
+    create_place_parser.add_argument('--description')
+    create_place_parser.add_argument('--start_at', default=time.mktime(datetime.datetime.now().timetuple()))
+    create_place_parser.add_argument('--finish_at', default=time.mktime(
+        (datetime.datetime.now() + datetime.timedelta(days=1)).timetuple()))
+    create_place_parser.add_argument('--order', type=int)
+    create_place_parser.add_argument('--lng', type=float)
+    create_place_parser.add_argument('--lat', type=float)
+    create_place_parser.set_defaults(handler='create_place')
+
+    update_place_parser = sub_parsers.add_parser('update_place')
+    update_place_parser.add_argument('--event_id', type=int)
+    update_place_parser.add_argument('--title')
+    update_place_parser.add_argument('--description')
+    update_place_parser.add_argument('--start_at', default=time.mktime(datetime.datetime.now().timetuple()))
+    update_place_parser.add_argument('--finish_at', default=time.mktime(
+        (datetime.datetime.now() + datetime.timedelta(days=1)).timetuple()))
+    update_place_parser.add_argument('--order', type=int)
+    update_place_parser.add_argument('--lng', type=float)
+    update_place_parser.add_argument('--lat', type=float)
+    update_place_parser.set_defaults(handler='update_place')
+
+    place_details_parser = sub_parsers.add_parser('place_details')
+    place_details_parser.add_argument('--event_id', type=int)
+    place_details_parser.add_argument('--place_id', type=int)
+    place_details_parser.set_defaults(handler='place_details')
+
+    delete_place_parser = sub_parsers.add_parser('delete_place')
+    delete_place_parser.add_argument('--event_id', type=int)
+    delete_place_parser.add_argument('--place_id', type=int)
+    delete_place_parser.set_defaults(handler='delete_place')
+
+    places_map_parser = sub_parsers.add_parser('places_map')
+    places_map_parser.set_defaults(handler='places_map')
 
 
 def main():
@@ -396,6 +682,13 @@ def main():
     add_event_parsers(sub_parsers)
     add_feedback_parsers(sub_parsers)
     add_accounts_parsers(sub_parsers)
+    add_step_parsers(sub_parsers)
+    add_participant_parsers(sub_parsers)
+    add_place_parsers(sub_parsers)
+
+    # TODO: add assignees parsers
+    # TODO: add order steps
+    # TODO: add order places
 
     args = parser.parse_args()
 
